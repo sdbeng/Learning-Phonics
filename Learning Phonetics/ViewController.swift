34f9fc30a2ee1304ee4be013235b4f5c6e57b0fc
Sergio Barrera <sdbeng@gmail.com>
2014-11-21 10:54:12 -0800

    Initial Commit

---
+++ Learning Phonetics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Phonetics
+//
+//  Created by admin 33 on 11/21/14.
+//  Copyright (c) 2014 sdbwebsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
