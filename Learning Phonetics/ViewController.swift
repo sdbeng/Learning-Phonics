338bdf1102ca8a24cca1aa2f9971b2020a350844
Sergio Barrera <sdbeng@gmail.com>
2014-11-21 21:45:33 -0800

    First check-in

--- Learning Phonetics/ViewController.swift
+++ Learning Phonetics/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //recordingLabel hidden by default, only enabled when record button is pressed
+        recordingLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: text recording in progress
+        
+        //TODO: record the user's voice
+        println("print record audio in progress...")
+        
+        recordingLabel.hidden = false
+    }
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingLabel.hidden = true
+        
+        println("I stopped recording!")
+    }
+    
+    
+    
+    
+    
 }
 
