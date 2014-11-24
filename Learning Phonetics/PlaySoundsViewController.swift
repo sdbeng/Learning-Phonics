574fd585f829c661b7c0f315da6850660d046423
Sergio Barrera <sdbeng@gmail.com>
2014-11-24 14:00:15 -0800

    Add a slow recording button to the 2nd scene

---
+++ Learning Phonetics/PlaySoundsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlaySoundsViewController.swift
+//  Learning Phonetics
+//
+//  Created by admin 33 on 11/24/14.
+//  Copyright (c) 2014 sdbwebsolutions. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    //outlets
+    
+    //actions
+    
+    @IBAction func slowRecording(sender: UIButton) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
